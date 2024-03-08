05e0c201d09a439197e9fc5638e0f29463f92c5f
zeynepcircir <81877446+zeynepcircir@users.noreply.github.com>
2024-03-08 17:28:46 +0300

    Agm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'agm';
+  constructor() {}
+    ngOnInit(): void {}
+    display: any;
+    center: google.maps.LatLngLiteral = {
+      lat: 22.2736308,
+      lng: 70.7512555
+  };
+  zoom = 6;
+    moveMap(event: google.maps.MapMouseEvent) {
+        if (event.latLng != null) this.center = (event.latLng.toJSON());
+    }
+    move(event: google.maps.MapMouseEvent) {
+        if (event.latLng != null) this.display = event.latLng.toJSON();
+    }
+   
+    mapOptions: google.maps.MapOptions = {
+      center: { lat: 38.9987208, lng: -77.2538699 },
+      zoom: 14,
+      mapTypeId:google.maps.MapTypeId.TERRAIN
+    }
+    marker = {
+      position: { lat: 39.963985, lng: 32.792914 },
+   }
 }
+
