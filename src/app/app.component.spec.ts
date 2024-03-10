3fa47f2cf7530f0aae75c34e41ec658decd925db
zeynepcircir <81877446+zeynepcircir@users.noreply.github.com>
2024-03-10 17:43:08 +0300

    Maps

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'agm'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('agm');
+   
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,31 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
-    ngOnInit(): void {}
-    display: any;
-    center: google.maps.LatLngLiteral = {
-      lat: 22.2736308,
-      lng: 70.7512555
-  };
-  zoom = 6;
-    moveMap(event: google.maps.MapMouseEvent) {
-        if (event.latLng != null) this.center = (event.latLng.toJSON());
-    }
-    move(event: google.maps.MapMouseEvent) {
-        if (event.latLng != null) this.display = event.latLng.toJSON();
-    }
-   
-    mapOptions: google.maps.MapOptions = {
-      center: { lat: 38.9987208, lng: -77.2538699 },
-      zoom: 14,
-      mapTypeId:google.maps.MapTypeId.TERRAIN
-    }
-    marker = {
-      position: { lat: 39.963985, lng: 32.792914 },
-   }
-}
+  display: any;
+  myLatLng = { lat: 48.829677, lng: 2.239609 };
 
+  markers: any[] = [];
+  polygons: any[] = [];
+
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.myLatLng,
+    zoom: 10,
+  };
+
+  move(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
+
+}
